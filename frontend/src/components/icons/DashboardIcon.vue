8b7244d749975b70f068135cc380ad5eafe7bdf9
Avandar2024 <3045487561@qq.com>
2025-05-28 11:22:55 +0800

    add login

---
+++ frontend/src/components/icons/DashboardIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path
+      fill="currentColor"
+      d="M21 18H3V6h18m0-2H3c-1.11 0-2 .89-2 2v12c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V6a2 2 0 0 0-2-2m-9 6.5A2.5 2.5 0 0 1 14.5 9A2.5 2.5 0 0 1 17 11.5a2.5 2.5 0 0 1-2.5 2.5A2.5 2.5 0 0 1 12 11.5m1 3h3c1.1 0 2 .9 2 2V17h-8v-.5c0-1.1.9-2 2-2h1Z"
+    />
+  </svg>
+</template>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+	plugins: [vue(), vueDevTools()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+			},
+		},
+	},
+});
