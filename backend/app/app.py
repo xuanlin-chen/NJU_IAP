e776f21ad9fdf0f3dfd61480ad1e21daa1408833
xuanlinchen <2862159448@qq.com>
2025-05-31 10:13:03 +0800

    final

--- backend/app/app.py
+++ backend/app/app.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     
     # 启用CORS以支持跨域请求
-    CORS(app)
+    CORS(app,resources={r"/*": {"origins": "http://47.122.71.85"}})
     
     # 设置JSON编码
     app.config['JSON_AS_ASCII'] = False
