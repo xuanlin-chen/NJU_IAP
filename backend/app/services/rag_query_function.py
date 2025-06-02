2f0a08e788ffb4d24fbbc651ba8201e459a6091b
Lagranye <915472419@qq.com>
2025-06-03 00:31:29 +0800

    get api to env

--- backend/app/services/rag_query_function.py
+++ backend/app/services/rag_query_function.py
@@ -1,9 +1,13 @@
 from dashscope import Application
 from http import HTTPStatus
+import os
+
+API_RAG = os.getenv("DASHSCOPE_API_KEY_RAG")
 
 def query_rag(question: str):
     config = {
-        'api_key': "sk-abf733f33ea64dcd9362d96bcfb77b6f",
+        # 'api_key': "sk-abf733f33ea64dcd9362d96bcfb77b6f",
+        'api_key': API_RAG,
         'app_id': '7c8a24304e1f4f4e943d4472904294de'
     }
         
